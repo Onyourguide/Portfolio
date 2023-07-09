7ff260606d4edf9fb84cdd029d942930ac7671ed
Onyourguide <thunyathorn2906@gmail.com>
2023-07-09 20:32:15 +0700

    HelloMind

---
+++ Code_Basic_Python/001.py
@@ -0,0 +1,10 @@
+"""Source code ' s docstring"""
+def funtionName ():
+    """Function's docstring"""
+    print("Output")
+
+funtionName()
+#1 อธิบายโปรแกรมทำอะไร (docstring)
+#2 def : Keyword คำสั่งสร้างฝังชั่น
+#2 funtionName : f(x) x = docstring
+#3 calling parameter : ()
