3a832a6a539ce5d98db6a0f7eaffb3576b128dd4
Onyourguide <thunyathorn2906@gmail.com>
2023-07-09 22:00:02 +0700

    Add

---
+++ Code_Basic_Python/006x.py
@@ -0,0 +1,11 @@
+"""Source code ' s docstring"""
+def funtion1 ():
+    """Function1's docstring"""
+    variable_1 = input()
+    function2(variable_1)
+
+def function2(parameter):
+    """Function2's docstring"""
+    print(parameter)
+
+funtion1()
---
+++ Code_Basic_Python/007.py
@@ -0,0 +1,17 @@
+"""Source code ' s docstring"""
+def funtionName ():
+    """FunctionName's docstring"""
+    variable_1 = input()
+    variable_2 = int(variable_1)
+
+    print("Type of variable 1", type(variable_1))
+    print("Type of variable 2", type(variable_2))
+    print("Output", variable_1 , variable_2+31)
+
+    print(variable_2)
+    print(variable_2+5)
+    print(variable_2-17)
+    print(variable_2*32)
+    print(variable_2*variable_2*5+(5*10)*(variable_2)+3)
+
+funtionName()
