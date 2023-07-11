ba4861e9034a46af25b9bde1d9a1d7b2726406cb
Onyourguide <thunyathorn2906@gmail.com>
2023-07-11 16:58:03 +0700

    Add

---
+++ Code_Basic_Python/029xx.py
@@ -0,0 +1,14 @@
+data1 = float(input())
+data2 = float(input())
+data3 = float(input())
+data4 = float(input())
+data5 = float(input())
+data6 = float(input())
+
+def Circula():
+    if data3 > abs(data4*2 or data5*2):
+        print("Yes")
+    else:
+        print("No")
+    
+Circula()
