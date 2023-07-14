87ccf388e45bb0bf617371fbb379d7786cecceef
Onyourguide <thunyathorn2906@gmail.com>
2023-07-14 19:06:45 +0700

    Add

---
+++ Code_Basic_Python/041-.py
@@ -0,0 +1,8 @@
+def BootSequence():
+    data = int(input())
+    count = 0
+    for count in range(data):
+        print(count + 1, end="_")
+BootSequence()
+
+#Output มี _ เกิน
---
+++ Code_Basic_Python/042-.py
@@ -0,0 +1,6 @@
+def Sequence():
+    data = int(input())
+    count = 0
+    for count in range(data):
+        print(count)
+Sequence()
