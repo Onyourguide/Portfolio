7c4b30cc39729d6957d7a1f56a982892949b0343
Onyourguide <thunyathorn2906@gmail.com>
2023-07-23 21:44:08 +0700

    Add

---
+++ Code_Basic_Python/047.py
@@ -0,0 +1,11 @@
+def Sequence5():
+
+    data = int(input())
+    for i in range (data):
+        a = i+1 
+        for j in range (a):
+            b = j+1
+            print(b , end=" ")
+        print()
+
+Sequence5()
